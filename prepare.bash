41853976c5ddb8e55305dee2538542f1ba6ff5fa
ibqn <evgen.ibqn@gmail.com>
2020-06-06 23:55:57 +0200

    update dependencies

--- prepare.bash
+++ prepare.bash
@@ -9,7 +9,7 @@ source './venv/bin/activate'
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade setuptools
 
-python3 -m pip install --upgrade --requirement './requirements.txt'
+python3 -m pip install --upgrade --requirement './dev-requirements.txt'
 
 # you might need to install
 # sudo apt install python3-venv
